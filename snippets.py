265efb59a6d3b17e0e100eaac1fb6a802138e2b5
Tatiana Tylosky <tatianatylosky@gmail.com>
2014-11-22 13:40:59 -0500

    split parser ito subparsers

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -21,3 +23,19 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+
+    argumemts = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
